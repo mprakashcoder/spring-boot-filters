282bf873815e31614449343b47afb20057ac82ac
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-10-06 08:49:59 +0530

    committed

---
+++ src/main/java/com/swsa/domain/Training.java
@@ -0,0 +1,21 @@
+package com.swsa.domain;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Training {
+
+    private int id;
+    private String name;
+    private int duration;
+    private String prerequisite;
+
+    public Training(String name, int duration, String prerequisite) {
+        this.name = name;
+        this.duration = duration;
+        this.prerequisite = prerequisite;
+    }
+}
