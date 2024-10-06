3ec7d14cd6bb01a0e79bc8fa23aa89695c69c1ff
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-10-06 09:04:19 +0530

    committed

---
+++ src/main/java/com/swsa/exceptions/NoTrainingsInDatabaseException.java
@@ -0,0 +1,5 @@
+package com.swsa.exceptions;
+
+public class NoTrainingsInDatabaseException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/swsa/exceptions/TrainingNotfoundException.java
@@ -0,0 +1,4 @@
+package com.swsa.exceptions;
+public class TrainingNotfoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
